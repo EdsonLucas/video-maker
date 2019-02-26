dcfe01804e9764b33c8170fd8ffc65f50b59bb09
Lucas <lucas@MacBook-Pro-de-Lucas.local>
2019-02-26 13:46:00 -0300

    Adicionando termo de busca e prefixo funcionando

---
+++ .history/index_20190226131956.js
@@ -0,0 +1,15 @@
+function start() {
+
+  const content = {}
+
+  content.searchTerm = askAndReturnSearchTerm()
+
+  function askAndReturnSearchTerm() {
+    return 'Term Example'
+  }
+
+  console.log(content)
+
+}
+
+start()
